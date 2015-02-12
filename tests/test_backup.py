375cec129af5a9122b6a2e9205685c533b71d0de
Sam Kuehn <samkuehn@gmail.com>
2015-02-11 17:43:43 -0700

    add very genreal smoke test

---
+++ tests/test_backup.py
@@ -0,0 +1,12 @@
+import unittest2 as unittest
+
+from backup import *  # noqa
+
+
+class BackupTestCase(unittest.TestCase):
+    def test_stub(self):
+        self.assertEqual(1, 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
